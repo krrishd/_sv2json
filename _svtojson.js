d47d50f2a91e1378432f25144e2b19b1ef6e265a
Krish Dholakiya <krishna.dholakiya@gmail.com>
2016-11-08 16:09:45 -0700

    Changed indentation to spaces.

--- _svtojson.js
+++ _svtojson.js
@@ -9,24 +9,24 @@
 */
 
 let _sv2json = (() => {
-    function convert(data, delimiter) {
-        let json = [];
-        let d = data.split('\n');
-        let keys = d[0].split(delimiter || ',');
-        d = d.splice(1);
-        d.forEach(data => {
-            let keyVal = data.split(delimiter || ',');
-            let obj = {};
-            keys.forEach((keyData, n) => {
-                obj[keyData] = keyVal[n];
-            });
-            json.push(obj);
-        });
-        return json;
-    } 
-    return function(data, delimiter) {
-        return convert(data, delimiter);
-    }
+  function convert(data, delimiter) {
+    let json = [];
+    let d = data.split('\n');
+    let keys = d[0].split(delimiter || ',');
+    d = d.splice(1);
+    d.forEach(data => {
+      let keyVal = data.split(delimiter || ',');
+      let obj = {};
+      keys.forEach((keyData, n) => {
+        obj[keyData] = keyVal[n];
+      });
+      json.push(obj);
+    });
+    return json;
+  } 
+  return function(data, delimiter) {
+    return convert(data, delimiter);
+  }
 })();
 
 module.exports = _sv2json;
